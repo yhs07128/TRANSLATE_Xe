34fb1dc674b1781e7801cedfec41c27dc2f3f7e5
zbeever <zach.beever@gmail.com>
2019-06-26 11:30:45 -0500

    Cleaned up code.

--- src/Electron.h
+++ src/Electron.h
@@ -27,12 +27,13 @@ public:
     int child_ions;
 
     std::vector<Electron<S>*> ionization_electrons;
-
     std::mt19937 &generator;
 
-    Electron(int &ions, double volts, std::mt19937 &gen):
-        ionized(ions), child_ions(0), x(0), v(random_velocity<S>(gen, true)), accel((e / m_e) * volts * 1e2), volts_per_cm(volts), total_time(0), generator(gen)
-    { energy = J_to_eV(0.5 * m_e * v * v); }
+    Electron(int &ions, double volts, S velocity, std::mt19937 &gen):
+        ionized(ions), child_ions(0), x(0), v(velocity), accel((e / m_e) * volts * 1e2), volts_per_cm(volts), total_time(0), generator(gen)
+    {
+        energy = J_to_eV(0.5 * m_e * v * v);
+    }
 
     int index(double v) const
     {
@@ -47,16 +48,24 @@ public:
         return ind;
     }
 
-    inline double x_sec_e(double v) const { return (is_gas) ? momentum_xsec_gas[index(v)] : effective_xsec_liquid[index(v)]; }
+    inline double x_sec_e(double v) const
+    {
+        return (is_gas) ? momentum_xsec_gas[index(v)] : effective_xsec_liquid[index(v)];
+    }
 
-    inline double x_sec_p(double v) const { return (is_gas) ? momentum_xsec_gas[index(v)] : momentum_xsec_liquid[index(v)]; }
+    inline double x_sec_p(double v) const
+    {
+        return (is_gas) ? momentum_xsec_gas[index(v)] : momentum_xsec_liquid[index(v)];
+    }
 
-    inline double x_sec_i(double v) const { return ionization_xsec[index(v)]; }
+    inline double x_sec_i(double v) const
+    {
+        return ionization_xsec[index(v)];
+    }
 
     inline double x_sec_ex(double v, int level) const
     {
-        switch (level)
-        {
+        switch (level) {
             case 11:
                 return excite_xsec_11[index(v)];
             case 13:
@@ -72,11 +81,11 @@ public:
 
     void remove_energy(double eV)
     {
-        energy -= eV / energy_transfer_efficiency;
-        std::uniform_real_distribution<double> dist(0.0, 1.0);
+        energy -= eV;
+        std::uniform_real_distribution<double> dist(8.0, 12.0);
 
         if (energy > 12)
-            energy = dist(generator) * 12;
+            energy = dist(generator);
 
         if (energy < 0)
             energy = 0;
@@ -84,24 +93,16 @@ public:
         v = sqrt((2 * eV_to_J(energy)) / m_e) * (v / abs(v));
     }
 
-    double K_max(double u)
-    {
-        if (use_const_kmax)
-            return K_max_;
-
-        return (is_gas) ? K_max_gas[index(u)] : K_max_liquid[index(u)];
-    }
-
     double probability(double u, double x_sec)
     {
-        double prob = (u * 1e2 * x_sec) / K_max(u);
+        double prob = (u * 1e2 * x_sec) / K_max;
         assert (prob < 1);
         return prob;
     }
 
     double next_collision(double u)
     {
-        double lambda = n * K_max(u);
+        double lambda = n * K_max;
         double beta = 1 / lambda;
 
         std::exponential_distribution<double> exponential(lambda);
@@ -134,40 +135,31 @@ public:
         double ex_14_prob = probability(u, x_sec_ex(u, 14));
         double ex_15_prob = probability(u, x_sec_ex(u, 15));
 
-        std::uniform_real_distribution<double> dist(0.0, 1.0);
-        double rand = dist(generator);
+        std::uniform_real_distribution<double> rand_event(0.0, 1.0);
+        double prob = rand_event(generator);
 
-        if (rand < ion_prob)
-        {
-            if (recursive)
-               ionization_electrons.push_back(new Electron<S>(ionized, volts_per_cm, generator));
+        if (prob < ion_prob) {
+            if (recursive) {
+                std::uniform_real_distribution<double> rand_starting_energy(2.0, 4.0);
+                ionization_electrons.push_back(new Electron<S>(ionized, volts_per_cm, rand_starting_energy(generator), generator));
+            }
 
             remove_energy(15.76);
 
             ionized++;
             child_ions++;
-        }
-        else if (rand < (ion_prob + ex_11_prob))
-        {
+        } else if (prob < (ion_prob + ex_11_prob)) {
             remove_energy(11.68);
-        }
-        else if (rand < (ion_prob + ex_11_prob + ex_13_prob))
-        {
+        } else if (prob < (ion_prob + ex_11_prob + ex_13_prob)) {
             remove_energy(13.21);
-        }
-        else if (rand < (ion_prob + ex_11_prob + ex_13_prob + ex_14_prob))
-        {
+        } else if (prob < (ion_prob + ex_11_prob + ex_13_prob + ex_14_prob)) {
             remove_energy(14.10);
-        }
-        else if (rand < (ion_prob + ex_11_prob + ex_13_prob + ex_14_prob + ex_15_prob))
-        {
+        } else if (prob < (ion_prob + ex_11_prob + ex_13_prob + ex_14_prob + ex_15_prob)) {
             remove_energy(15.23);
-        }
-        else if (rand < (ion_prob + ex_11_prob + ex_13_prob + ex_14_prob + ex_15_prob + col_prob))
-        {
-            S n = random_unit_vector<S>(generator);
+        } else if (prob < (ion_prob + ex_11_prob + ex_13_prob + ex_14_prob + ex_15_prob + col_prob)) {
+            S unit_vec = random_unit_vector<S>(generator);
 
-            S a = (M_a * u) * n;
+            S a = (M_a * u) * unit_vec;
             S b = (m_e) * v;
             S c = (M_a) * vm;
 
@@ -175,7 +167,9 @@ public:
 
             double r = x_sec_p(u) / std::max(x_sec_e(u), x_sec_p(u));
 
-            if (dist(generator) < r)
+            std::uniform_real_distribution<double> rand_momentum_change(0.0, 1.0);
+
+            if (rand_momentum_change(generator) < r)
                 v = v1;
             else
                 v = abs(v1) / abs(v) * v;
@@ -194,34 +188,34 @@ void print_data_line(Electron<T> &elec, std::ofstream &file)
 }
 
 template<>
-void print_data_line<Vec>(Electron<Vec> &elec, std::ofstream &file)
+void print_data_line(Electron<Vec> &elec, std::ofstream &file)
 {
     file << elec.total_time * 1e9 << "," << elec.x.x * 1e6 << "," << elec.x.y * 1e6 << "," << elec.x.z * 1e6 << "," << elec.energy << "," << (elec.x.x / elec.total_time) << "," << elec.ionized << "\n";
     return;
 }
 
 template<typename T>
-void generate_plot(int volts, double cutoff, int number, int write_every)
+void generate_plot(int volts, double cutoff, int number, int write_every, int group, std::string folder)
 {
     int ions = 0;
     std::mt19937 generator(std::chrono::system_clock::now().time_since_epoch().count());
-    Electron<T> elec(ions, volts, generator);
-    std::ofstream file("../py/simulation-runs/1d/" + std::to_string(volts) + "V - " + std::to_string(number) + ".txt");
+    Electron<T> elec(ions, volts, random_velocity<T>(generator, true), generator);
+    std::ofstream file;
+
+    file = std::ofstream("../py/simulation-runs/" + folder + "/" + std::to_string(volts) + "V - " + std::to_string(number) + ".txt");
     
     int simulation_step = 1;
-    const int total_progress_steps = 5;
+    const int total_progress_steps = 100;
     std::array<bool, total_progress_steps> progress;
     progress.fill(false);
     int progress_count = 1;
 
-    while (elec.total_time < cutoff)
-    {
+    while (elec.total_time < cutoff) {
         elec.update();
 
-        if (((elec.total_time / cutoff) > (float(progress_count) / total_progress_steps)) && progress[(progress_count++ - 1)] == false)
-        {
+        if (((elec.total_time / cutoff) > (float(progress_count) / total_progress_steps)) && progress[(progress_count++ - 1)] == false) {
             progress[progress_count - 2] = true;
-            std::cout << "." << std::flush;
+            std::cout << "\rWriting " << volts << "V file group " << group << " - " << progress_count - 1 << "%" << std::flush;
         }
 
         if (simulation_step++ % write_every != 0)
@@ -232,44 +226,6 @@ void generate_plot(int volts, double cutoff, int number, int write_every)
 
     elec.update();
     print_data_line<T>(elec, file);
-
-    file.close();
-
-    return;
-}
-
-template<>
-void generate_plot<Vec>(int volts, double cutoff, int number, int write_every)
-{
-    int ions = 0;
-    std::mt19937 generator(std::chrono::system_clock::now().time_since_epoch().count());
-    Electron<Vec> elec(ions, volts, generator);
-    std::ofstream file("../py/simulation-runs/3d/" + std::to_string(volts) + "V - " + std::to_string(number) + ".txt");
-    
-    int simulation_step = 1;
-    const int total_progress_steps = 5;
-    std::array<bool, total_progress_steps> progress;
-    progress.fill(false);
-    int progress_count = 1;
-
-    while (elec.total_time < cutoff)
-    {
-        elec.update();
-
-        if (((elec.total_time / cutoff) > (float(progress_count) / total_progress_steps)) && progress[(progress_count++ - 1)] == false)
-        {
-            progress[progress_count - 2] = true;
-            std::cout << "." << std::flush;
-        }
-
-        if (simulation_step++ % write_every != 0)
-            continue;
-        
-        print_data_line<Vec>(elec, file);
-    }
-
-    elec.update();
-    print_data_line<Vec>(elec, file);
 
     file.close();
 
--- src/main.cpp
+++ src/main.cpp
@@ -19,8 +19,7 @@ int main()
     int dimensions;
     std::cout << "1D or 3D: ";
     std::cin >> dimensions;
-    while (std::cin.fail() || (dimensions != 1 && dimensions !=3))
-    {
+    while (std::cin.fail() || (dimensions != 1 && dimensions !=3)) {
         std::cin.clear();
         std::cin.ignore();
         std::cin >> dimensions;
@@ -40,8 +39,7 @@ int main()
     double cutoff;
     std::cout << "Stop After (s): ";
     std::cin >> cutoff;
-    while (std::cin.fail())
-    {
+    while (std::cin.fail()) {
         std::cin.clear();
         std::cin.ignore();
         std::cin >> cutoff;
@@ -50,8 +48,7 @@ int main()
     int write_every;
     std::cout << "Record Every n Measurements: ";
     std::cin >> write_every;
-    while (std::cin.fail())
-    {
+    while (std::cin.fail()) {
         std::cin.clear();
         std::cin.ignore();
         std::cin >> write_every;
@@ -63,27 +60,24 @@ int main()
     int batches;
     std::cout << "Batches (in groups of " + std::to_string(cores) + "): ";
     std::cin >> batches;
-    while (std::cin.fail())
-    {
+    while (std::cin.fail()) {
         std::cin.clear();
         std::cin.ignore();
         std::cin >> batches;
     }
 
-    for (int j = 0; j < volts_list.size(); j++)
-    {
-        for (int i = 0; i < batches; i++)
-        {
-            std::cout << "Writing " << int(volts_list[j]) << "V file group " << i + 1 << " " << std::flush;
+    for (int j = 0; j < volts_list.size(); j++) {
+        for (int i = 0; i < batches; i++) {
+            std::cout << "Writing " << int(volts_list[j]) << "V file group " << i + 1 << " - 0%" << std::flush;
             std::thread branches[cores];
 
-            for (int k = 0; k < cores; k++)
-            {
+            for (int k = 0; k < cores; k++) {
                 if (dimensions == 1)
-                    branches[k] = std::thread(generate_plot<double>, int(volts_list[j]), cutoff, 4 * i + (k + 1), write_every);
+                    branches[k] = std::thread(generate_plot<double>, int(volts_list[j]), cutoff, 4 * i + (k + 1), write_every, i + 1, "1d");
                 else
-                    branches[k] = std::thread(generate_plot<Vec>, int(volts_list[j]), cutoff, 4 * i + (k + 1), write_every);   
+                    branches[k] = std::thread(generate_plot<Vec>, int(volts_list[j]), cutoff, 4 * i + (k + 1), write_every, i + 1, "3d");   
             }
+            
             for (int k = 0; k < cores; k++)
                     branches[k].join();
 
